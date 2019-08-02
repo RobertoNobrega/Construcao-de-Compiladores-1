3d5f645795295ca82220585404df96c5c8e685ef
AlissonJ <alissonjorgepb@gmail.com>
2019-08-02 16:00:10 -0300

    Update AnalisadorSintatico2.java

--- Nota_1/Analisador_Sintatico/src/AnalisadorSintatico2.java
+++ Nota_1/Analisador_Sintatico/src/AnalisadorSintatico2.java
@@ -14,19 +14,14 @@ public class AnalisadorSintatico2 {
 		//program		Palavra-Chave			1
 		//teste			Identificador			1
 	}
-	public void checarProgram(String token) {	//program(palavra reservada) identificador e ;
-		if(token.contentEquals("program")) {
+	public void checarProgram(Token token) {	//program(palavra reservada) identificador e ;
+		if((token.getToken()).contentEquals("program")) throw new SintaticoException("program", token);
 			//ler o proximo
-			if(token.contentEquals("Identificador")) {
+			if((token.getClassificacao()).contentEquals("Identificador"));
 				//ler o proximo(classificação do token) segunda string do buffer, ou classifica novamente o string?
-				if(token.contentEquals(";")) {
-					//ler o proximo
-				}
-			}
-		}			
+				if((token.getToken()).contentEquals(";"));
+		}			//passou os 3 ifs, metodoProgram está CORRETO, caso falhe em algum já lança erro
 		
-		//passou os 3 ifs, metodoProgram está CORRETO, caso falhe em algum já lança erro
-	}
 	public void checarDeclaracaoVar(Token token) {
 		if((token.getToken()).contentEquals("var")) {
 			checarListaDecVar(token);
@@ -34,24 +29,21 @@ public class AnalisadorSintatico2 {
 			//nenhuma variavel declarada -> continue
 		}
 	}
-	private void checarListaDecVar(Token token) {
-		final List<String> Lista_tipos = Arrays.asList("integer","real","boolean");//checarTipo()
-		if((token.getClassificacao()).contentEquals("Identificador")){
-			
-			if((token.token).contentEquals(":"))  {//throw
-				if(Lista_tipos.contains(token)) {	//Tipo = integer|real|boolean
-					
+	private void checarListaDecVar(Token token) {//  valor2: integer;
+												//   begin
+		checarListaID(token);
+		if( !(token.getToken()).contentEquals(":") ) throw new SintaticoException(":", token);
+			checarTipo(token);
+			if( !(token.getToken()).contentEquals(";") ) throw new SintaticoException(";", token);
+				//pode ser um ID ou pode passar pra proxima parte ->Se token = ID então checarListaDecVar; se não continui
+				if( (token.getClassificacao()).contentEquals("Identificador") ) {
+					checarListaDecVar(token);
+				}else {
+					//Continue
 				}
-			}else {
-				//era esperado ":" Erro!!
-			}
-				
-		}else {
-			//erro, tem que ter no minimo 1 ID
-		}
 	}
 	public void checarListaID(Token token) {
-		//chamar checarID() e checarListaId
+		//chamar checarID() e checarListaID
 		checarID(token);
 		if( (token.token).contentEquals(",") ) { //mais de um identificador
 			//proximo armazeneTokenBuffer()
@@ -62,12 +54,14 @@ public class AnalisadorSintatico2 {
 	}
 	public static void checarTipo(Token token) {
 		final List<String> Lista_tipos = Arrays.asList("integer","real","boolean");
-		if( !(Lista_tipos.contains(token.getToken())) ) throw new SintaticoException("Tipo",token);
+		if( !(Lista_tipos.contains(token.getToken())) ) throw new SintaticoException("integer || real || boolean",token);
 			System.out.println("Tipo ok!");
+			//armazeneTokenBuffer()
 	}
 	public static void checarID(Token token) {
 		if( !(token.getClassificacao()).contentEquals("Identificador") ) throw new SintaticoException("Identificador",token);
 			System.out.println("Identificador ok!");
+			//armazeneTokenBuffer();
 	}
 
 	
