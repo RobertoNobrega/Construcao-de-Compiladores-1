5125c3ad70c6e69699c098ec0d8384a1a5262362
Roberto Nóbrega <r_t_4@hotmail.com>
2019-08-04 21:16:35 -0300

    AS1.5

--- Nota_1/Analisador_Sintatico/src/Analisador_Sintatico.java
+++ Nota_1/Analisador_Sintatico/src/Analisador_Sintatico.java
@@ -57,6 +57,30 @@ public class Analisador_Sintatico{
 		System.out.println("\n\tQuantidade de Objetos >> " + buffer.size());
 	}
 	
+	public boolean op_multiplicativo(String token){
+		if(token.equals("*") || token.equals("/") || token.equals("and"))
+		   return true;
+		return false;
+	}
+	
+	public boolean op_aditivo(String token){
+		if(token.equals("+") || token.equals("-") || token.equals("or"))
+		   return true;
+		return false;
+	}
+	
+	public boolean op_relacional(String token){
+		if(token.equals("=") || token.equals("<") || token.equals(">") || token.equals("<=") || token.equals(">=") || token.equals("<>"))
+		   return true;
+		return false;
+	}
+	
+	public boolean sinal(String token){
+		if(token.equals("+") || token.equals("-"))
+			return true;
+		return false;
+	}
+	
 	public void lista_de_parametros_linha() throws SintaticoException{
 	   if(!buffer.isEmpty() && buffer.get(0).getToken().equals(";")){
 		  buffer.remove(0);
@@ -132,8 +156,126 @@ public class Analisador_Sintatico{
 	   }
 	}
 	
+	public boolean variavel(){
+		if(buffer.get(0).getClassificacao().equals("Identificador"))
+		  return true;
+		return false;
+	}
+	
+	public void fator() throws SintaticoException{
+		if(!buffer.isEmpty()){
+		   if(buffer.get(0).getClassificacao().equals("Identificador")){
+			  buffer.remove(0);
+			  if(!buffer.isEmpty() && buffer.get(0).getClassificacao().equals("(")){
+				buffer.remove(0);
+				lista_de_expressoes();
+				if(!buffer.isEmpty() && buffer.get(0).getClassificacao().equals(")")){
+				   
+				}
+			  }
+		   }else if(buffer.get(0).getClassificacao().equals("Número Inteiro") || buffer.get(0).getClassificacao().equals("Número Real")
+			 || buffer.get(0).getClassificacao().equals("true") || buffer.get(0).getClassificacao().equals("false")){
+			   
+		   }else if(buffer.get(0).getToken().equals("(")){
+			   
+		   }else if(buffer.get(0).getToken().equals("not")){
+			   
+		   }
+		}
+	}
+	
+	public void termo() throws SintaticoException{
+		fator();
+	}
+	
+	public void expressao_simples() throws SintaticoException{
+		termo();
+	}
+	
+	public void expressao() throws SintaticoException{
+		expressao_simples();
+	}
+	
+	public void lista_de_expressoes() throws SintaticoException{
+	   expressao();
+	}
+	
+	public void metodo_C() throws SintaticoException{
+	   if(!buffer.isEmpty() && buffer.get(0).getToken().equals("(")){
+		  buffer.remove(0);
+		  lista_de_expressoes();
+		  
+	   }else if(buffer.isEmpty()){
+		  System.out.println("\n\tO Buffer está Vazio no método_C.");
+	   }
+	}
+	
+	public void ativacao_procedimento() throws SintaticoException{
+		if(!buffer.isEmpty() && buffer.get(0).getClassificacao().equals("Identificador")){
+		   buffer.remove(0);
+		   metodo_C();
+		}else if(buffer.isEmpty()){
+		   System.out.println("\n\tO Buffer está Vazio no método ativacao_procedimento.");
+		}else{
+		   throw new SintaticoException("Identificador", buffer.get(0).getClassificacao());
+		}
+	}
+	
+	public void comando() throws SintaticoException{
+		if(!buffer.isEmpty()){
+		   if(variavel()){
+			  if(buffer.size() > 1){
+				 //buffer.remove(0);
+				 if(buffer.get(1).getToken().equals(":=")){
+				    buffer.remove(0);
+				    buffer.remove(0);
+				    expressao();
+				 }else{
+					ativacao_procedimento();
+				 }
+			  }else{
+				buffer.remove(0);
+			  }
+		   }else if(buffer.get(0).getToken().equals("begin")){
+			  comando_composto();
+		   }else if(buffer.get(0).getToken().equals("if") || buffer.get(0).getToken().equals("while")){
+			  buffer.remove(0);
+			  expressao();
+		   }else{
+			  throw new SintaticoException("Identificador ou begin ou if ou while", buffer.get(0).getToken());
+		   }
+		}else{
+		   System.out.println("\n\tO Buffer está Vazio no método comando .");
+		}
+	}
+	
+	public void lista_de_comandos() throws SintaticoException{
+		if(!buffer.isEmpty()){
+		   comando();
+		   c
+		}
+	}
+	
+	public void comandos_opcionais() throws SintaticoException{
+		lista_de_comandos();
+	}
+	
 	public void comando_composto() throws SintaticoException{
-		// >> Continuar Aqui.
+		if(!buffer.isEmpty() && buffer.get(0).getToken().equals("begin")){
+		   buffer.remove(0);
+		   comandos_opcionais();
+		   if(!buffer.isEmpty() && buffer.get(0).getToken().equals("end")){
+			  buffer.remove(0);
+		   }else if(buffer.isEmpty()){
+			 System.out.println("\n\tO Buffer está Vazio no método comando_composto.");
+		   }else{
+			 throw new SintaticoException("end", buffer.get(0).getToken());
+		   }
+		}else if(buffer.isEmpty()){
+		   System.out.println("\n\tO Buffer está Vazio no método comando_composto.");
+		}else{
+		   throw new SintaticoException("begin", buffer.get(0).getToken());
+		}
 	}
 	
 	public void declaracao_de_subprograma() throws SintaticoException{
