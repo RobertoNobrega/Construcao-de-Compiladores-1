b3767e7e8f088a04b6c23d520d1729b7f6185aa2
AlissonJ <alissonjorgepb@gmail.com>
2019-08-07 08:27:48 -0300

    Update AnalisadorSintatico2.java
    subprograma

--- Nota_1/Analisador_Sintatico/src/AnalisadorSintatico2.java
+++ Nota_1/Analisador_Sintatico/src/AnalisadorSintatico2.java
@@ -1,3 +1,4 @@
+package Sintatico;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,28 +16,34 @@ public class AnalisadorSintatico2 {
 		//teste			Identificador			1
 	}
 	public void checarProgram(Token token) {	//program(palavra reservada) identificador e ;
-		if((token.getToken()).contentEquals("program")) throw new SintaticoException("program", token);
+		if( !(token.getToken()).contentEquals("program") ) throw new SintaticoException("program", token);
 			//ler o proximo
-			if((token.getClassificacao()).contentEquals("Identificador"));
-				//ler o proximo(classificação do token) segunda string do buffer, ou classifica novamente o string?
-				if((token.getToken()).contentEquals(";"));
-		}			//passou os 3 ifs, metodoProgram está CORRETO, caso falhe em algum já lança erro
+			System.out.println("program lido!");
+			checarID(token);
+			//ler o proximo(classificação do token) segunda string do buffer, ou classifica novamente o string?
+			if( !(token.getToken()).contentEquals(";") ) throw new SintaticoException(";", token);
+				checarDeclaracaoVar(token);
+				//checarDecSubprograma()
+				//checarComandoComp()
+				//if( !(token.getToken()).contentEquals(".") ) throw new SintaticoException(".",token);	//Final do arquivo
+	}			//passou os 3 ifs, metodoProgram está CORRETO, caso falhe em algum já lança erro
 		
 	public void checarDeclaracaoVar(Token token) {
 		if((token.getToken()).contentEquals("var")) {
+			System.out.println("var encontrado!");
 			checarListaDecVar(token);
 		}else {
+			System.out.println("sem var!");
 			//nenhuma variavel declarada -> continue
 		}
 	}
-	private void checarListaDecVar(Token token) {//  valor2: integer;
-												//   begin
+	private void checarListaDecVar(Token token) {
 		checarListaID(token);
 		if( !(token.getToken()).contentEquals(":") ) throw new SintaticoException(":", token);
 			checarTipo(token);
 			if( !(token.getToken()).contentEquals(";") ) throw new SintaticoException(";", token);
 				//pode ser um ID ou pode passar pra proxima parte ->Se token = ID então checarListaDecVar; se não continui
-				if( (token.getClassificacao()).contentEquals("Identificador") ) {
+				if( (token.getClassificacao()).contentEquals("Identificador") ) { //pode ou nao ter outro ID
 					checarListaDecVar(token);
 				}else {
 					//Continue
@@ -45,7 +52,7 @@ public class AnalisadorSintatico2 {
 	public void checarListaID(Token token) {
 		//chamar checarID() e checarListaID
 		checarID(token);
-		if( (token.token).contentEquals(",") ) { //mais de um identificador
+		if( (token.getToken()).contentEquals(",") ) { //mais de um identificador
 			//proximo armazeneTokenBuffer()
 			checarListaID(token);
 		}else {
@@ -62,6 +69,23 @@ public class AnalisadorSintatico2 {
 		if( !(token.getClassificacao()).contentEquals("Identificador") ) throw new SintaticoException("Identificador",token);
 			System.out.println("Identificador ok!");
 			//armazeneTokenBuffer();
+	}
+	public void checarDeclaracaoSubprogramas(Token token) {//certo?
+		checarDeclaracaoSubprograma(token); //tem o primeiro procedure obrigatorio
+		if( !(token.getToken()).contentEquals(";") ) throw new SintaticoException(";", token);
+			checarDeclaracaoSubprogramas(token);
+	}
+	public void checarDeclaracaoSubprograma(Token token) {
+		if( !(token.getToken()).contentEquals("procedure") ) throw new SintaticoException("procedure", token);//tirar excecao,se nao for procedure, será vazio
+			checarID(token);
+			checarArgumentos(token);
+			if( !(token.getToken()).contentEquals(";") ) throw new SintaticoException(";", token);
+				checarDeclaracaoVar(token);
+				//
+	}
+	private void checarArgumentos(Token token) {
+		if( !(token.getToken()).contentEquals("(") ) throw new SintaticoException("(", token);
+		//
 	}
 
 	
